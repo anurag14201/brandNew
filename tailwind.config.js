0684f79ab06f7ed92f61a7a75b60e949ca400ab7
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2023-03-25 23:00:26 +0600

    Nops

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    // ...
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        // screens: {
+        //   "big": "1024px",
+        //   "bxl": "1280px",
+        //   "bmd": "768px",
+        //   "bsm": "640px",
+        //   'bxxs': '340px', // small-width
+        // },
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: false,
+    },
+}
