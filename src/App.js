33a718a156f37d6ea7aec27e03022646e75e96f5
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2023-03-25 22:49:41 +0600

    ICON

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Navbar from './Section/NavBar';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-
+      <Routes>
+        <Route path='/' element={<Navbar></Navbar>}></Route>
+      </Routes>
     </div>
   );
 }
